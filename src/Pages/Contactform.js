33ef2b76cbf0a764e6f041067be33b6068047d41
godwinchinkwe <chinkwegodwin@gmail.com>
2024-02-16 14:34:14 +0100

    signs

--- src/Pages/Contactform.js
+++ src/Pages/Contactform.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState}from 'react'
 import "./Contactform.css"
 import { MdOutlineMarkEmailUnread } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
@@ -8,11 +8,31 @@ import { AiOutlineEyeInvisible } from "react-icons/ai";
 
 
 const Contactform = () => {
+  // const [inputs, setInputs] = useState({});
+
+  const [ inputs, setInputs] = useState({
+    name: "",
+     email: "",
+     password:"",
+     admin: false,
+     required:"",
+   });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
 
 
   return (
     <div className='contactform_container'>
-      <div className='contactform_wrap'>
+      <div className='contactform_wrap' onSubmit={handleSubmit}>
         <div className='sign_topcontent'>
          <h1 className='sign_text'>Sign Up</h1>
          <div className='underline'></div>
@@ -21,21 +41,42 @@ const Contactform = () => {
         <div className='input_wrap'>
         <div className='input_sign'>
         <IoPerson className='signup_icon'/>
-          <input className='inputss' placeholder='Name'/>
+          <input className='inputss'
+           placeholder='Name'
+           type='text'
+           name="name"
+           required="true"
+           value={inputs.name || ""} 
+           onChange={handleChange}
+           />
         </div>
+
         <div className='input_sign'>
           <MdOutlineMarkEmailUnread className='signup_icon'/>
-          <input className="inputss" placeholder='Email Id'/>
+          <input className="inputss" 
+          placeholder='Email Id'
+          type='email'
+          name="email"
+          value={inputs.email || ""} 
+           onChange={handleChange}/>
         </div>
+
+
         <div className='input_sign'>
         <RiLockPasswordLine className='signup_icon' />
-          <input className='inputss' placeholder='Password'/>
+          <input className='inputss' 
+          placeholder='Password'
+          name='password'
+          type='number'
+          value={inputs.password || ""} 
+          onChange={handleChange}
+          />
           <AiOutlineEye className='passwordeye' /> <AiOutlineEyeInvisible className='passwordclose'/> 
         </div>
        </div>
        
         <div className='signupbutton'>
-          <button className='btn_sign'>Sign Up</button>
+          <button className='btn_sign' type="submit">Sign Up</button>
           <button className='btn_login'>Login </button>
         </div>
       </div>
